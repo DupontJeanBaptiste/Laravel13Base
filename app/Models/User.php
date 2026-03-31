c78d3a713f827adb6c2c8d0344b2b50e2b2b3747
DupontJeanBaptiste <jbdupont67@gmail.com>
2026-03-31 19:26:28 +0200

    EDIT : Forget to make the trait HasRoles usable by adding it into @use

--- app/Models/User.php
+++ app/Models/User.php
@@ -16,7 +16,7 @@ use Spatie\Permission\Traits\HasRoles;
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, HasRoles, Notifiable;
 
     /**
      * Get the attributes that should be cast.
